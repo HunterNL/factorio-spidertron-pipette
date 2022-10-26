c10b656e458f33388701e10356248b988aae3519
Hunter <hunternl@users.noreply.github.com>
2022-10-26 10:49:39 +0200

    use custom input instead of pipette event

---
+++ data.lua
@@ -0,0 +1,8 @@
+local remote_picker_input = {
+    type = 'custom-input',
+    name = 'hunter:remote-picker',
+    key_sequence = 'SHIFT + Q',
+    action = "lua"
+}
+
+data:extend({remote_picker_input})
